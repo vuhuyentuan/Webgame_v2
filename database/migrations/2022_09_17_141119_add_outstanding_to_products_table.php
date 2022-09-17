f9bbe65a3345f457fd7d2618b08d0f78ff6daaeb
levantri1920 <76199425+levantri1920@users.noreply.github.com>
2022-09-17 21:15:57 +0700

    nhẹ

--- database/migrations/2022_09_17_141119_add_outstanding_to_products_table.php
+++ database/migrations/2022_09_17_141119_add_outstanding_to_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddViewsToProducts extends Migration
+class AddOutstandingToProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,8 @@ class AddViewsToProducts extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
-            //
+            $table->integer('outstanding')->default(0)->after('image_detail');
+            $table->bigInteger('views')->default(0)->after('outstanding');
         });
     }
 
